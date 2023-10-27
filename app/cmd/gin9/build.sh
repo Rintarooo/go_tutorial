fa0553fc6f430d1a840ab4f48d92d1f88820f55c
Rintarooo <310rnomeado@gmail.com>
2023-10-27 16:07:53 +0900

    feat: add GET req for simple external API

--- app/cmd/gin9/build.sh
+++ app/cmd/gin9/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -rf go.mod go.sum
 go mod init main
 go mod tidy
--- app/cmd/gin9/main.go
+++ app/cmd/gin9/main.go
@@ -5,69 +5,35 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	// "fmt"
+	"io/ioutil"
 )
-
-type snacks struct{
-	ID		string `json:"id"`
-	Title	string `json:"title"`
-}
-
-var snack1 = []snacks{
-	{ID: "1", Title: "chocolate"},
-	{ID: "2", Title: "pancake"},
-}
-
-func getJsonById(ctx *gin.Context) {
-	// id, err := strconv.Atoi(ctx.Param("id"))
-
-	// URL(Path) parameter. http://localhost:3000/2
-	id := ctx.Param("id")
-
-	// Query parameter. http://localhost:3000/?id=2
-	// id := ctx.Query("id")
-	
-	for _, k := range snack1 {
-		if k.ID == id {
-			// ctx.IndentedJSON(http.StatusOK, k)
-			ctx.IndentedJSON(http.StatusOK, gin.H{"message": "ok", "ID": k.ID, "Title": k.Title})
-			// ctx.IndentedJSON(http.StatusOK, gin.H{"Hello! ID: %s\n": "This ID's title: %s", k.ID, k.Title})
-			// ctx.IndentedJSON(http.StatusOK, gin.H{"Hello! ID: ", k.ID, k.Title})
-			// ctx.String(http.StatusOK, "Hello! ID: %s\nThis ID's title: %s", k.ID, k.Title)
-			// ctx.String(http.StatusOK, "Hello! title: %s",k.Title)
-			return
-		}
-	}
-	ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "id not found"})
-}
-
 
 func openMainPage(ctx *gin.Context) {
 	ctx.HTML(200, "index.html", nil)
-
-	// ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found index.html"})
 }
 
-func getPostById(ctx *gin.Context){
-  // ctx.HTML(200, "index.html", gin.H{"data": data})
-	id := ctx.PostForm("id")
-	for _, k := range snack1 {
-		if k.ID == id {
-			// ctx.IndentedJSON(http.StatusOK, gin.H{"message": "found your id", "ID": k.ID, "Title": k.Title})
-			ctx.HTML(200, "post_id.html", gin.H{"message": "found your id", "ID": k.ID, "Title": k.Title})
+func getItunesApi(ctx *gin.Context){
+	res, err := http.Get("http://httpbin.org/get")
+	if err != nil{
+		ctx.IndentedJSON(500, gin.H{"error message: ": err.Error()})
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+			ctx.IndentedJSON(500, gin.H{"message": err.Error(),})
 			return
-		}
-	}	
-	ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "id not found", "ID":"", "Title":""})
+	}
+
+	ctx.String(200, string(body))
+
 }
 
 func main(){
-	// fmt.Println("Hello!")
 	router := gin.Default()
-	// router.Get("/")
-	router.LoadHTMLGlob("./htmls/*.html")
+	router.LoadHTMLGlob("./templates/*.html")
 	router.GET("/", openMainPage)
-	router.GET("/:id", getJsonById)
-	router.POST("/post_id", getPostById)
+	router.GET("/api_songs", getItunesApi)
 	router.Run()
 }
